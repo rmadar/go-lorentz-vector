829c028fbd307927bfa1c4a58d45837f7392865f
rmadar <romain.madar@gmail.com>
2020-03-23 23:52:19 +0100

    Changing  to

--- lv/lorentzvector.go
+++ lv/lorentzvector.go
@@ -53,63 +53,63 @@ func (v FourVec) String() string {
 }
 
 // Get Px
-func (v *FourVec) Px() float64 {
+func (v FourVec) Px() float64 {
 	return v.Pvec.X
 }
 
 // Get Py
-func (v *FourVec) Py() float64 {
+func (v FourVec) Py() float64 {
 	return v.Pvec.Y
 }
 
 // Get Pz
-func (v *FourVec) Pz() float64 {
+func (v FourVec) Pz() float64 {
 	return v.Pvec.Z
 }
 
 // Get E
-func (v *FourVec) E() float64 {
+func (v FourVec) E() float64 {
 	return v.P4
 }
 
 // Transverse momentum
-func (v *FourVec) Pt() float64 {
+func (v FourVec) Pt() float64 {
 	px, py := v.Pvec.X, v.Pvec.Y
 	return math.Sqrt(px*px + py*py)
 }
 
 // Get Eta
-func (v *FourVec) Eta() float64 {
+func (v FourVec) Eta() float64 {
 	p, pz := v.P(), v.Pz()
 	return 0.5 * math.Log((p+pz)/(p-pz))
 }
 
 // Get Phi
 // FIX-ME: need to check if the s1.Angle is [0, 2pi] as HEP convention
-func (v *FourVec) Phi() float64 {
+func (v FourVec) Phi() float64 {
 	pt := r3.Vector{v.Px(), v.Py(), 0}
 	Ox := r3.Vector{1, 0, 0}
 	return Ox.Angle(pt).Radians()
 }
 
 // Get rapidity
-func (v *FourVec) Rapidity() float64 {
+func (v FourVec) Rapidity() float64 {
 	e, pz := v.E(), v.Pz()
 	return 0.5 * math.Log((e+pz)/(e-pz))
 }
 
 // Squared distance of the 3-vector
-func (v *FourVec) P2() float64 {
+func (v FourVec) P2() float64 {
 	return v.Pvec.Norm2()
 }
 
 // Distance of the 3-vector
-func (v *FourVec) P() float64 {
+func (v FourVec) P() float64 {
 	return v.Pvec.Norm()
 }
 
 // Transverse energy
-func (v *FourVec) Et() float64 {
+func (v FourVec) Et() float64 {
 	e2 := v.E() * v.E()
 	pt2 := v.Pt() * v.Pt()
 	p2 := v.P2()
@@ -117,36 +117,36 @@ func (v *FourVec) Et() float64 {
 }
 
 // Lorentz scalar product
-func (v *FourVec) Dot(u FourVec) float64 {
+func (v FourVec) Dot(u FourVec) float64 {
 	pv, pu := v.Pvec, u.Pvec
 	return u.P4*v.P4 - pv.Dot(pu)
 }
 
 // Invariant mass ('lorentz norm' of the 4-vector)
-func (v *FourVec) M() float64 {
-	return math.Sqrt(v.Dot(*v))
+func (v FourVec) M() float64 {
+	return math.Sqrt(v.Dot(v))
 }
 
 // Get DeltaR
-func (v *FourVec) DeltaR(u FourVec) float64 {
+func (v FourVec) DeltaR(u FourVec) float64 {
 	dphi := v.Phi() - u.Phi()
 	deta := v.Eta() - u.Eta()
 	return math.Sqrt(dphi*dphi + deta*deta)
 }
 
 // Get DeltaPhi
-func (v *FourVec) DeltaPhi(u FourVec) float64 {
+func (v FourVec) DeltaPhi(u FourVec) float64 {
 	return math.Acos(math.Cos(v.Phi() - u.Phi()))
 }
 
 // Get 3D boost
-func (v *FourVec) GetBoost() r3.Vector {
+func (v FourVec) GetBoost() r3.Vector {
 	return v.Pvec.Mul(1. / v.P4)
 }
 
 // Apply Lorentz boost
 // (FIX-ME: improve notation since gamma2 != gamma*gamma)
-func (v *FourVec) ApplyBoost(b r3.Vector) FourVec {
+func (v FourVec) ApplyBoost(b r3.Vector) FourVec {
 
 	// Transformation parameters
 	v_p := v.Pvec
@@ -163,7 +163,7 @@ func (v *FourVec) ApplyBoost(b r3.Vector) FourVec {
 }
 
 // Four-vector addition
-func (v *FourVec) Add(vec FourVec) FourVec {
+func (v FourVec) Add(vec FourVec) FourVec {
 	return FourVec{
 		Pvec: v.Pvec.Add(vec.Pvec),
 		P4:   v.P4 + vec.P4,
@@ -171,7 +171,7 @@ func (v *FourVec) Add(vec FourVec) FourVec {
 }
 
 // Four-vector multiplication with a scalar
-func (v *FourVec) Multiply(a float64) FourVec {
+func (v FourVec) Multiply(a float64) FourVec {
 	return FourVec{
 		Pvec: v.Pvec.Mul(a),
 		P4:   a * v.P4,
