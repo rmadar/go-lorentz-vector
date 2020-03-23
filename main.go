c9fc35876df8c4f2672ad324cef69ff9fabe65de
rmadar <romain.madar@gmail.com>
2020-03-23 11:50:51 +0100

    Adding a comparison with fmom

--- main.go
+++ main.go
@@ -1,9 +1,29 @@
 package main
 
-import "fmt"
-import "localtest/madar/lv"
+import (
+	"fmt"
+	"localtest/madar/lv"
+
+	// To compare with go-hep-fmom
+	"go-hep.org/x/hep/fmom"
+)
 
 func main(){
+
+	// Quick checks
+	fmt.Println("\nPrinting some 4-vector manipulations")
+	fmt.Println("====================================")
+	quickTest()
+	
+
+	// Cross-check with fmom
+	fmt.Println("\n\nComparison with fmom")
+	fmt.Println("====================")
+	checkAgainstFmom(1., 2., 3., 4.)
+}
+
+
+func quickTest(){
 	
 	// Create an object using (px, py, pz, E)
 	vec1 := lv.NewFourVecPxPyPzE(1., 2., 3., 4.)
@@ -29,8 +49,17 @@ func main(){
 	fmt.Println(boost_RF)
 }
 
-
 func print_4vec(v lv.FourVec) {
 	fmt.Println(v.Px(), v.Py(), v.Pz(), v.E())
 	fmt.Println(v.M())
 }
+
+func checkAgainstFmom(px, py, pz, e float64){
+	p_fmom := fmom.NewPxPyPzE(px, py, pz, e)
+	p_here := lv.NewFourVecPxPyPzE(px, py, pz, e)
+	fmt.Println("Pt  : lv=", p_here.Pt(), "vs fmom=", p_fmom.Pt())
+	fmt.Println("Eta : lv=", p_here.Eta(), "vs fmom=", p_fmom.Eta())
+	fmt.Println("Phi : lv=", p_here.Phi(), "vs fmom=", p_fmom.Phi())
+	fmt.Println("Mass: lv=", p_here.M(), "vs fmom=", p_fmom.M())
+}
+
