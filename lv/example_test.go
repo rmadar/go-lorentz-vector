79f20d1081c274af2cdfcccfd7b5b2fcdd8370e3
rmadar <romain.madar@gmail.com>
2020-03-25 08:47:30 +0100

    Adding function examples

--- lv/example_test.go
+++ lv/example_test.go
@@ -6,10 +6,19 @@ import (
 	"github.com/rmadar/go-lorentz-vector/lv"
 )
 
-func ExampleFourVec_pxPyPzE() {
-	p1 := lv.NewFourVecPxPyPzE(1, 2, 3, 4)
-	fmt.Printf("p1=%v\n", p1)
+func ExampleFourVec_NewFourVecPxPyPzE() {
+	p := lv.NewFourVecPxPyPzE(1, 2, 3, 4)
+	fmt.Printf("p = %v\n", p)
 
 	// Output:
-	// p1=FourVec{Px: 1, Py: 2, Pz: 3, E:4, M:1.4142135623730951}
+	// p = FourVec{Px: 1, Py: 2, Pz: 3, E:4, M:1.4142135623730951}
+}
+
+
+func ExampleFourVec_NewFourVecPtEtaPhiM() {
+	p := lv.NewFourVecPtEtaPhiM(1, 2, 3, 4)
+	fmt.Printf("p = %v\n", p)
+
+	// Output:
+	// p = FourVec{Px: -0.9899924966004454, Py: 0.1411200080598672, Pz: 3.626860407847019, E:5.491276392425375, M:3.999999999999999}
 }
